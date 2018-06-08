04bf3ce9b149c38cbad23c141809227da9cae187
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-08 19:45:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # person.rb
+class Person
+  def initialize 
+    @adele_goldberg = adele_goldberg
+    @alan_kay = alan_kay
+  end
+  
+  # def adele_goldberg 
+  #   @adele_goldberg
+  # end
+  
+  # def alan_kay
+  #   @alan_kay
+  # end
+  
+end
